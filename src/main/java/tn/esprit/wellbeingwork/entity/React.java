de912da486d90b28d8b66b44c2a8ed660a646caa
ibtissem-lengliz <74663330+ibtissem-lengliz@users.noreply.github.com>
2022-07-22 19:26:52 +0100

    Update React.java

--- src/main/java/tn/esprit/wellbeingwork/entity/React.java
+++ src/main/java/tn/esprit/wellbeingwork/entity/React.java
@@ -22,7 +22,7 @@ public class React implements Serializable {
     private long idReact;
 
     //value= 0 dislike value=1 like
-    private boolean likedislike;
+    private boolean like;
 
     @ManyToOne
     private User userReact;
