7b146f8857527c6703fefd8f778d46cfb44d5800
ilengliz@vermeg.com <ilengliz@vermeg.com>
2022-07-23 14:58:11 +0100

    last commit about jwt auth

--- src/main/java/tn/esprit/wellbeingwork/WellbeingworkApplication.java
+++ src/main/java/tn/esprit/wellbeingwork/WellbeingworkApplication.java
@@ -21,25 +21,27 @@ public class WellbeingworkApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WellbeingworkApplication.class, args);
 	}
-
 	@Bean
-	CommandLineRunner run(UserService userService) {
-		return args -> {
-			userService.saveRole(new Role(null, "ROLE_USER"));
-			userService.saveRole(new Role(null, "ROLE_ADMIN"));
-			User user = new User();
-			user.setFirstName("ibtissem");
-			user.setLastName("lengliz");
-			user.setPositionName(tn.esprit.wellbeingwork.entity.Position.SoftwareDeveloper);
-			user.setEmail("ilengliz@gmail.com");
-			user.setPassword("030897");
-			userService.addRoleToUser(user.getEmail(), "ROLE_USER");
-			userService.saveUser(user);
-
-		};
-	}
-	@Bean
-	PasswordEncoder passwordEncoder() {
+	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
+
+
+//	@Bean
+//	CommandLineRunner run(UserService userService) {
+//		return args -> {
+//			userService.saveRole(new Role(null, "ROLE_USER"));
+//			userService.saveRole(new Role(null, "ROLE_ADMIN"));
+//			User user = new User();
+//			user.setFirstName("ibtissem");
+//			user.setLastName("lengliz");
+//			user.setPositionName(tn.esprit.wellbeingwork.entity.Position.SoftwareDeveloper);
+//			user.setEmail("ilengliz@gmail.com");
+//			user.setPassword("030897");
+//			userService.addRoleToUser(user.getEmail(), "ROLE_ADMIN");
+//			userService.saveUser(user);
+//
+//		};
+//	}
+	
 }
